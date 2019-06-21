919332f6d831be1018fbff8d5bdfcaf125e4b029
Mihai Stan <stanmihai17cs@gmail.com>
2019-06-21 16:50:37 +0200

    Add project containing the business logic

---
+++ AgpWps/AgpWps.Model/ViewModels/AddServerPopupViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Input;
+
+namespace AgpWps.Model.ViewModels
+{
+    public class AddServerPopupViewModel : ViewModelBase
+    {
+
+        private ICommand _addConnectionCommand;
+        public ICommand AddConnectionCommand
+        {
+            get => _addConnectionCommand;
+            set => Set(ref _addConnectionCommand, value);
+        }
+
+        private void AddServer(string serverUrl)
+        {
+            
+        }
+
+    }
+}
