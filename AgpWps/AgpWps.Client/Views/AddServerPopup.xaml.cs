06929f920e5d79fa271cdbb27eed09be03b279cc
Mihai Stan <stanmihai17cs@gmail.com>
2019-06-21 16:49:27 +0200

    Add server addition popup window

---
+++ AgpWps/AgpWps.Client/Views/AddServerPopup.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace AgpWps.Client.Views
+{
+    /// <summary>
+    /// Interaction logic for AddServerPopup.xaml
+    /// </summary>
+    public partial class AddServerPopup : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public AddServerPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
