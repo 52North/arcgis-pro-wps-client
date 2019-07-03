a02c25690ae4acf2db0bdbeb5c1771695873b214
Mihai Stan <stanmihai17cs@gmail.com>
2019-07-03 19:05:43 +0200

    Add rectangle model with viewmodel and control

--- AgpWps/AgpWps.Model/Geometry/Rectangle.cs
+++ AgpWps/AgpWps.Model/Geometry/Rectangle.cs
@@ -1,19 +1,17 @@
-﻿namespace AgpWps.Model.Geometry
+﻿using System;
+
+namespace AgpWps.Model.Geometry
 {
     public class Rectangle
     {
-        public Rectangle((double, double) leftBottom, (double, double) leftTop, (double, double) rightBottom, (double, double) rightTop)
+        public Rectangle(Tuple<double, double> leftBottom, Tuple<double, double> rightTop)
         {
             LeftBottom = leftBottom;
-            LeftTop = leftTop;
-            RightBottom = rightBottom;
             RightTop = rightTop;
         }
 
-        public (double, double) LeftBottom { get; }
-        public (double, double) LeftTop { get; }
-        public (double, double) RightBottom { get; }
-        public (double, double) RightTop { get; }
+        public Tuple<double, double> LeftBottom { get; }
+        public Tuple<double, double> RightTop { get; }
 
     }
 }
