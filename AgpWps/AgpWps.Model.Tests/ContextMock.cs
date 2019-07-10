416e5108e2a6cfd4a57163e5260b38f57b6f9ee9
Mihai Stan <stanmihai17cs@gmail.com>
2019-07-10 13:22:16 +0200

    Update test project dependencies and add mock services

---
+++ AgpWps/AgpWps.Model.Tests/ContextMock.cs
@@ -0,0 +1,13 @@
+﻿using AgpWps.Model.Services;
+using System;
+
+namespace AgpWps.Model.Tests
+{
+    public class ContextMock : IContext
+    {
+        public void Invoke(Action action)
+        {
+            action?.Invoke();
+        }
+    }
+}
---
+++ AgpWps/AgpWps.Model.Tests/DialogServiceMock.cs
@@ -0,0 +1,40 @@
+﻿using AgpWps.Model.Enums;
+using AgpWps.Model.Services;
+using AgpWps.Model.ViewModels;
+using System;
+
+namespace AgpWps.Model.Tests
+{
+    public class DialogServiceMock : IDialogService
+    {
+        public void ShowMessageDialog(string title, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowMessageDialog(string title, string message, DialogMessageType messageType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowExecutionBuilderDialog(ExecutionBuilderViewModel vm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowAddServerDialog()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShowFileSelectionDialog(string title, string filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ShowFileSaveDialog(string title, string filter = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
