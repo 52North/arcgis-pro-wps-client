e7138e285d25c29cf47e7b6caeeb8aa77a00e477
Mihai Stan <stanmihai17cs@gmail.com>
2019-07-11 17:00:21 +0200

    Add get only property to identity the input

--- AgpWps/AgpWps.Model/Exceptions/NullInputException.cs
+++ AgpWps/AgpWps.Model/Exceptions/NullInputException.cs
@@ -4,10 +4,11 @@ namespace AgpWps.Model.Exceptions
 {
     public class NullInputException : Exception
     {
+        public string InputName { get; }
 
         public NullInputException(string inputName) : base($"The provided input of name {inputName} cannot be null.")
         {
-
+            InputName = inputName;
         }
 
     }
