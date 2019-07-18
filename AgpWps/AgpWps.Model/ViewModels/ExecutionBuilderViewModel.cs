37aad045e68b481eb51bf80d000afe1795ccdc07
Mihai Stan <stanmihai17cs@gmail.com>
2019-07-18 17:33:12 +0200

    Add WPS process execution with file saving

--- AgpWps/AgpWps.Model/ViewModels/ExecutionBuilderViewModel.cs
+++ AgpWps/AgpWps.Model/ViewModels/ExecutionBuilderViewModel.cs
@@ -7,8 +7,10 @@ using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Windows.Input;
+using Wps.Client.Models;
 using Wps.Client.Services;
 
 namespace AgpWps.Model.ViewModels
@@ -30,11 +32,22 @@ namespace AgpWps.Model.ViewModels
             set => Set(ref _windowsLoadedCommand, value);
         }
 
-        private ICommand _executeCommand;
-        public ICommand ExecuteCommand
+        private RelayCommand _executeCommand;
+        public RelayCommand ExecuteCommand
         {
             get => _executeCommand;
             set => Set(ref _executeCommand, value);
+        }
+
+        private bool _isExecutingProcess;
+        public bool IsExecutingProcess
+        {
+            get => _isExecutingProcess;
+            set
+            {
+                Set(ref _isExecutingProcess, value);
+                ExecuteCommand.RaiseCanExecuteChanged();
+            }
         }
 
         private ObservableCollection<DataInputViewModel> _dataInputViewModels = new ObservableCollection<DataInputViewModel>();
@@ -72,15 +85,79 @@ namespace AgpWps.Model.ViewModels
             Title = _processId;
 
             WindowLoadedCommand = new RelayCommand(OnWindowsLoaded);
-            ExecuteCommand = new RelayCommand(ExecuteProcess);
+            ExecuteCommand = new RelayCommand(ExecuteProcess, () => !IsExecutingProcess);
         }
 
         private void ExecuteProcess()
         {
             try
             {
+                IsExecutingProcess = true;
+
                 var request = _requestFactory.CreateExecuteRequest(_processId, DataInputViewModels.ToList(),
                     DataOutputViewModels.ToList());
+
+                _wpsClient.AsyncGetDocumentResultAs<string>(_wpsUri, request).ContinueWith((task) =>
+                {
+                    var session = task.Result;
+
+                    session.Polled += (sender, args) =>
+                    {
+                        // TODO
+                    };
+
+                    session.Failed += (sender, args) =>
+                    {
+                        _dialogService.ShowMessageDialog("Failed",
+                            "Something went wrong when executing the process. Please check the server or verify that your input is valid.");
+                    };
+
+                    session.Finished += (sender, args) =>
+                    {
+                        _dialogService.ShowMessageDialog("Finished", $"The job {args.Result.JobId} has finished its execution. You can now access the outputs. ");
+
+                        foreach (var output in args.Result.Outputs)
+                        {
+                            var outputVm = DataOutputViewModels.FirstOrDefault(ovm => ovm.Identifier.Equals(output.Id));
+                            if (outputVm != null)
+                            {
+                                var outputFileExists = File.Exists(outputVm.FilePath);
+                                if (outputFileExists)
+                                {
+                                    var outputDirectory = Path.GetDirectoryName(outputVm.FilePath);
+                                    if (outputDirectory != null)
+                                    {
+                                        var random = new Random();
+                                        var newFileName = $"{output.Id}_{random.Next()}";
+                                        var fileName = Path.GetFileName(outputVm.FilePath);
+
+                                        var newFilePath = Path.Combine(outputDirectory, newFileName);
+                                        File.WriteAllText(newFilePath, output.Data);
+
+                                        _dialogService.ShowMessageDialog("Existing file", $"A file under the name {fileName} existed already, the output {output.Id} has been saved under {newFileName}.");
+                                    }
+                                }
+                                else
+                                {
+                                    File.WriteAllText(outputVm.FilePath, output.Data);
+                                }
+                            }
+                        }
+                    };
+
+                    var sessionPollingTask = session.StartPolling();
+                    var status = _wpsClient.GetJobStatus(_wpsUri, session.JobId).Result;
+
+                    if (status.Status == JobStatus.Failed)
+                    {
+                        _dialogService.ShowMessageDialog("Error", "The job could not be started, please try again.",
+                            DialogMessageType.Error);
+                        sessionPollingTask.Dispose();
+                        return;
+                    }
+
+                    sessionPollingTask.Wait();
+                });
             }
             catch (NoOutputSelectedException)
             {
@@ -95,6 +172,10 @@ namespace AgpWps.Model.ViewModels
             catch (Exception)
             {
                 _dialogService.ShowMessageDialog("Error", "An unexpected error has occured, please try again.");
+            }
+            finally
+            {
+                IsExecutingProcess = false;
             }
         }
 
