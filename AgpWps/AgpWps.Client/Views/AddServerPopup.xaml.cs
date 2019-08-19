8d1e7e095b0b193e7d36b0b13330856202ab08a4
Mihai Stan <stanmihai17cs@gmail.com>
2019-08-19 14:48:29 +0200

    Add placeholder text on the wps server popup dialog

--- AgpWps/AgpWps.Client/Views/AddServerPopup.xaml.cs
+++ AgpWps/AgpWps.Client/Views/AddServerPopup.xaml.cs
@@ -1,18 +1,51 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace AgpWps.Client.Views
 {
     /// <summary>
     /// Interaction logic for AddServerPopup.xaml
     /// </summary>
-    public partial class AddServerPopup : ArcGIS.Desktop.Framework.Controls.ProWindow
+    public partial class AddServerPopup
     {
+
+        private const string PlaceholderText = "Server URL";
+
         public AddServerPopup()
         {
             InitializeComponent();
-            ServerInputComboBox.Focus();
+            ServerInputComboBox.LostKeyboardFocus += ServerInputComboBoxOnLostKeyboardFocus;
+            ServerInputComboBox.GotKeyboardFocus += ServerInputComboBoxOnGotKeyboardFocus;
+            ServerInputComboBox.Text = PlaceholderText;
+            ServerInputComboBox.Foreground = Brushes.Gray;
 
             AddConnectionButton.Click += (sender, args) => Close();
+        }
+
+        private void ServerInputComboBoxOnGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is ComboBox box)
+            {
+                if (box.Text.Equals(PlaceholderText, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    box.Text = string.Empty;
+                    box.Foreground = Brushes.Black;
+                }
+            }
+        }
+
+        private void ServerInputComboBoxOnLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (sender is ComboBox box)
+            {
+                if (string.IsNullOrEmpty(box.Text) || box.Text.Replace(" ", "").Length == 0)
+                {
+                    box.Text = PlaceholderText;
+                    box.Foreground = Brushes.Gray;
+                }
+            }
         }
 
         private void AddServerPopupKeyDown(object sender, KeyEventArgs e)
